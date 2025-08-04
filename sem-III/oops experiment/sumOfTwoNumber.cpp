6d662ff5b0d34fc7855126e7c883e4910deb4309
arunprasa9968 <arunprasa9968@gmail.com>
2025-08-04 09:46:34 +0530

    4/8/2025

---
+++ sem-III/oops experiment/sumOfTwoNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ,b,sum;
+    cout<<"enter the two positive integer number ";
+    cin>>a>>b;
+    sum = a+b;
+    cout<<"the sum of two number : "<<sum<<endl;
+    return 0;
+    
+
+}
